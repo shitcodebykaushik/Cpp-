71a432923f3d1da7a5e488e3220b54198923d6cc
kaushik raj <122168016+shitcodebykaushik@users.noreply.github.com>
2024-01-03 15:16:31 +0530

    Add files via upload

---
+++ Loop.cpp
@@ -0,0 +1,19 @@
+/*Loops
+Where the condition is getting.
+What condition is geting tested
+Chage the value that is getting tested
+Automatic change of value tha is getting tested
+i++ it means use it then increament it 
+++i it means increament it then use it 
+1.while loop stands for  execute a target statement as long as a given condition is true. 
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int arr1[]={1,2,3,4,5,6,7,8,9};
+    int l1=0;
+    while (l1<9){
+        cout<<"Current number is "<<arr1[l1]<<endl;
+        l1++;
+    }
+}
