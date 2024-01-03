96c8609d630f5f66b64bbb96418d7b4fc49492b7
kaushik raj <122168016+shitcodebykaushik@users.noreply.github.com>
2024-01-03 16:14:53 +0530

    Update Loop.cpp

--- Loop.cpp
+++ Loop.cpp
@@ -18,3 +18,50 @@ int main(){
         l1++;
     }
 }
+/*Do while  this is basically used in sesor designing 
+ Here first condition is executed then it is tested
+ */
+#include <iostream>
+using namespace std;
+int main(){
+    int arr2[]={1,2,3,4,5,6,7,8,9};
+    int l2=0;
+    do {
+        cout<<"Cureent number is "<<arr2[l2]<<endl;
+        l2++;  
+    } while (l2<1);
+    {
+        cout<<"The current value is "<<endl;
+        
+    
+    }
+}
+ /*
+ A for loop is a repetition control structure that allows you to efficiently write a loop that needs to execute a specific number of times.
+intitlization
+condition
+increament 
+in range based for loop we use : instead of ; 
+
+ */
+#include<iostream>
+using namespace std;
+int main(){
+int my_number[]={1,2,3,4,5,6,7,8,9};
+for (int i=0;i<9;i++){
+    cout<<"The value are "<<my_number[i]<<endl;
+}
+ for(int i:my_number){
+    cout<<"Here is your value "<<i<<endl;
+ };
+};
+//Charactere breakdown loop
+#include<iostream>
+using namespace std;
+int main(){
+    char my_name[]="Kaushik Raj";
+    for(int i=0; my_name[i]!=0;i++){
+        cout<<"Charactr list are "<<my_name[i]<<endl;
+    }
+ return 0;
+}
