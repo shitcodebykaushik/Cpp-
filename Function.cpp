8309c370861b0c22cf5a4f9075dc7931eae21ae3
kaushik raj <122168016+shitcodebykaushik@users.noreply.github.com>
2024-01-08 06:59:37 +0530

    Update Function.cpp

--- Function.cpp
+++ Function.cpp
@@ -1,4 +1,14 @@
-//Functionreturn type function name (){ statement }
+//Inside the classes these function are called method.
+//Main is the the first enetry point .Its automatically called .It is kind of function
+// Design a function
+           //return type (what i will give back )
+          // function name (name of function)
+         // () [what you have to give me ]
+       //{} [inside this we gotta write the statement 
+//  call by value here we pass the copy of variable 
+//  call by reference
+// The function’s code is stored in only one place in memory,
+
 #include<iostream>
 using namespace std;
 char name (){
